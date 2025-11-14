46deb7630e196f3d857616a9640747810421359d
Gulesh <gshukla@haverford.edu>
2025-11-14 17:10:38 -0600

    update readme with steps to run the project

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# Awesome Project Build with TypeORM
+# Steps to build and run the project 
 
-Steps to run this project:
+1. Start by, cloning the git repository
+2. Run `npm i` command inside the root directory
+3. Setup database settings inside `data-source.ts` file
+4. Run `npm start` command
 
-1. Run `npm i` command
-2. Setup database settings inside `data-source.ts` file
-3. Run `npm start` command
+# Using the CLI:
+
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { incomingSourceDB, outgoingSourceDB } from "./data-source"
 import {initial_full_load} from "./migration/initial_full_load";
 import {sync_job} from "./job/sync_job";
+import {validation_job} from "./job/validation_job";
 
 
 async function initializeDatabases() {
@@ -12,10 +13,11 @@ async function initializeDatabases() {
 
 initializeDatabases().then(r =>
 {
-    //initial_full_load().then( () => {console.log('initial load successful...!!!')});
+    initial_full_load().then( () => {console.log('initial load successful...!!!')});
     sync_job().then(()=> {
         console.log("sync run!")
-    })
+    });
+    validation_job().then(()=> {console.log('validation job...!!!')});
 }).catch(error => console.log(error));
 
 
